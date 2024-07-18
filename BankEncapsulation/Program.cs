e60b6038ff33df5b5617713c14256d81d0a30dd0
CherieMillan <cherie99millan@gmail.com>
2024-07-18 15:36:45 -0400

    Made a BankAccount class that contains how much user has depoisted and what their balance is

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            string input =Console.ReadLine();
+
+            double amount;
+            if (double.TryParse(input, out amount))
+            {
+                account.Deposit(amount);
+            }
+
+        Console.WriteLine("Your balance is " + account.GetBalance());
+
         }
     }
 }
